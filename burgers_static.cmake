5d45b5c895faafb0dedb01defbbb6b0a1f4e7e15
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-11-21 00:11:34 +1300

    Create burgers_static.cmake

---
+++ burgers_static.cmake
@@ -0,0 +1,4 @@
+set(TEST_GIT_REPO https://github.com/OpenCMISS-Examples/burgers_static.git)
+set(TEST_BRANCH develop)
+set(TEST_TARGETS burgers_static_fortran)
+set(TEST_EXPECTED_RESULTS expected_results.txt)
